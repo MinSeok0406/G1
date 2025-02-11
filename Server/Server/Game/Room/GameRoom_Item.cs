557d0b3d11ed282cc48622562fa5eeedefdea1bd
MinSeok0406 <hen0406@naver.com>
2025-02-11 17:34:49 +0900

    서버

---
+++ Server/Server/Game/Room/GameRoom_Item.cs
@@ -0,0 +1,13 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game
+{
+	public partial class GameRoom : JobSerializer
+	{
+		
+	}
+}
