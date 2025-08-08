77fb455815d15008945dfb0cb1a1d99f8f896fb0
LDJ5098 <ldj5098@gmail.com>
2025-08-09 03:52:15 +0900

    파일 정리

--- DongJu/장난감 정리/color_script/ObjectColor.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class ObjectColor : MonoBehaviour
-{
-    [Header("이 장난감이 속한 색상 그룹")]
-    public ToyColorCategory colorCategory;
-}
--- DongJu/장난감 정리/color_script/ToyColorCategory.cs
+++
@@ -1,6 +0,0 @@
-public enum ToyColorCategory
-{
-    Red,
-    Blue,
-    Yellow
-}
